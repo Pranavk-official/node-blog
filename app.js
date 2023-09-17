5a71fb330d7cb515dd50cf49ce08785b8b2481e5
Pranav K <pranavkcse@gmail.com>
2023-09-17 20:15:10 +0530

    register user feature added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
 
 // DB connection variable
 const connectDB = require('./server/config/db');
@@ -15,6 +18,17 @@ connectDB();
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+// cookies & session
+app.use(cookieParser());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    savedUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+}))
+
 // Static Folder
 app.use(express.static('public'))
 
@@ -24,6 +38,7 @@ app.set('layout' , './layouts/main')
 app.set('view engine' , 'ejs')
 
 app.use('/', require('./server/routes/main'))
+app.use('/', require('./server/routes/admin'))
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
